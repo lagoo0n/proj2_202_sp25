87a67aa91a92654f3e2229ef2a29b6a1f95e68d7
lagoo0n <loganchook@gmail.com>
2026-05-03 23:17:23 -0700

    task 4

--- proj2.py
+++ proj2.py
@@ -45,19 +45,51 @@ columns = [
     "total_co2_emissions_excluding_lucf_per_capita",
 ]
 
+numbers = set(columns) - {"country"}
+
 def filter_rows(data:Optional[Node], 
                 field_name:str,
                 comparison: str,
                 value: Union[str,float,int]
                 ) -> Optional[Node]:
+    
     if data is None:
         return None
-    
     if field_name not in columns:
         raise ValueError("Invalid Field Name")
-    
     if comparison not in ["less_than", "greater_than", "equal"]:
         raise ValueError("Invalid Comparison")
     if field_name == "country" and comparison != "equal":
         raise ValueError("Invalid Comparison for country")
 
+    filtered_next = filter_rows(data.next, field_name, comparison, value)
+    
+    parts = data.value.data.split(",")
+    field_index = columns.index(field_name)
+    field_value = parts[field_index]
+
+    correct = False
+
+    if field_value == "":
+        return filtered_next
+    
+    if field_name in numbers:
+        field_value = float(field_value)
+        value = float(value)
+
+    if comparison == "equal":
+        if field_value == value:
+            correct = True
+
+    elif comparison == "less_than":
+        if field_value < value:
+            correct = True
+
+    else:
+        if field_value > value:
+            correct = True
+
+    if correct:
+        return Node(data.value, filtered_next)
+    
+    return filtered_next
