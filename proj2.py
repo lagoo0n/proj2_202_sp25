d950e2b2d957631fb74524560444f108c83ca248
lagoo0n <loganchook@gmail.com>
2026-05-03 22:34:47 -0700

    edits

--- proj2.py
+++ proj2.py
@@ -3,11 +3,24 @@ import math
 from dataclasses import dataclass
 from typing import *
 
+import sys
+sys.setrecursionlimit(10_000)
 
-# Put your data definitions first!
+@dataclass(frozen=True)
+class Row:
+    data: float
 
-# ...
+@dataclass(frozen=True)
+class Node:
+    value: Row
+    next: Node | None
 
-# Then your functions.
-
-# ...
+def listlen(data: Optional[Node]) -> int:
+    if data is None:
+        return 0
+    else:
+        return 1 + listlen(data.next)
+    
+def filter_rows(data:Optional[Node], 
+                field_name:str,
+                comparison: str)
